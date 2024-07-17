eebea2ad23db1da488c1b2915b880a9cdd78f5ed
ednakiruai <edna.kiruai@student.moringaschool.com>
2024-07-18 02:35:27 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,55 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message_array_asc = Message.query.order_by(Message.created_at).all()
+        response_body =[message.to_dict() for message in message_array_asc]
+        return make_response(response_body, 200)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response_body = new_message.to_dict()
+        
+        return make_response(response_body, 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
-
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        response_body = {"message": "Message not found!"}
+        return make_response(response_body, 404)
+        
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data.get(attr))
+            
+        db.session.add(message)
+        db.session.commit()
+        
+        response_body = message.to_dict()
+        return make_response(response_body, 200)
+    
+    elif request.method == 'DELETE':
+        
+        db.session.delete(message)
+        db.session.commit()
+        
+        response_body = {"message": "Successfully deleted!"}
+        
+        return make_response(response_body, 200)
+        
+        
+        
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
